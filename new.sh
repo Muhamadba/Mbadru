410944a832d8a13017395f53e6ed7b23987b73f0
Muhamadba <48536350+Muhamadba@users.noreply.github.com>
2019-03-21 05:55:51 +0700

    Update new.sh

--- new.sh
+++ new.sh
@@ -25,7 +25,7 @@ echo
 echo $i"────────────────────────────────    ─────"
 echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" B"$i" |"
 echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    |"$cy" E"$i" |"
-echo $i"|"$me" 3"$i" |"$cy" Dark-FB Version5    "$i"     |"$i"         |"$cy" N"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" Dark-FB Version5         "$i"|"$i"    |"$cy" N"$i" |"
 echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
 echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" K"$i" |"
 echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" E"$i" |"
@@ -261,8 +261,8 @@ echo $cy"Terima Kasih Sudah Pakai Tool Saya"
 sleep 2
 echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
 sleep 2
-echo $ku"WhatsApp :"$i" 085835787069"
-echo $ku"Facebook :"$i" Riski Darmawan"
+echo $ku"WhatsApp :"$i" 0895375872878"
+echo $ku"Facebook :"$i" MUHAMAD BADRU"
 sleep 2
 echo $pur">> Thanks Yang Sudah Support Saya <<"
 exit
